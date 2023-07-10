83b3ac70aed548db16966c7fe6cc190c14b607d8
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-07-10 16:56:14 +0200

    added toggleTaskDone function

--- src/App.js
+++ src/App.js
@@ -33,6 +33,15 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+      return task;
+    }));
+  };
+
   return (
     <>
       <Container>
@@ -50,7 +59,7 @@ function App() {
           body={
             <>
               <Searchbar />
-              <TasksList hideDone={hideDone} tasks={tasks} removeTask={removeTask} />
+              <TasksList hideDone={hideDone} tasks={tasks} removeTask={removeTask} toggleTaskDone={toggleTaskDone} />
             </>
           }
         />
--- src/TasksList/index.js
+++ src/TasksList/index.js
@@ -1,11 +1,11 @@
 import { List, Task, Content, ToggleDoneButton, EditButton, RemoveButton } from "./styled";
 
-export const TasksList = ({ tasks, removeTask, hideDone }) => {
+export const TasksList = ({ tasks, removeTask, hideDone, toggleTaskDone }) => {
   return (
     <List>
       {tasks.map((task) => (
         <Task hidden={hideDone && task.done} key={task.id}>
-          <ToggleDoneButton>{task.done ? "✔️" : ""}</ToggleDoneButton>
+          <ToggleDoneButton onClick={() => toggleTaskDone(task.id)}>{task.done ? "✔️" : ""}</ToggleDoneButton>
           <Content>{task.content}</Content>
           <EditButton>✏️</EditButton>
           <RemoveButton onClick={() => removeTask(task.id)}>🗑️</RemoveButton>
