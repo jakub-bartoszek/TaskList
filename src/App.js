9d7149050b5cb9d32871cd13edb4826733a8178d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-07-12 14:26:18 +0200

    added custom hook for tasks

--- src/App.js
+++ src/App.js
@@ -10,56 +10,75 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 function App() {
-	const [hideDone, setHideDone] = useState(false);
-	const [tasks, setTasks] = useState(
-		JSON.parse(localStorage.getItem("tasks"))
-	);
+	const useTasks = () => {
+		const [tasks, setTasks] = useState(
+			JSON.parse(localStorage.getItem("tasks"))
+		);
 
-	useEffect(() => {
-		localStorage.setItem("tasks", JSON.stringify(tasks));
-	}, [tasks]);
+		useEffect(() => {
+			localStorage.setItem("tasks", JSON.stringify(tasks));
+		}, [tasks]);
+
+		const addNewTask = (newTaskContent) => {
+			setTasks((tasks) => [
+				...tasks,
+				{
+					id: tasks.length
+						? tasks[tasks.length - 1].id + 1
+						: 1,
+					done: false,
+					content: newTaskContent
+				}
+			]);
+		};
+
+		const removeTask = (id) => {
+			setTasks((tasks) =>
+				tasks.filter((task) => task.id !== id)
+			);
+		};
+
+		const toggleTaskDone = (id) => {
+			setTasks((tasks) =>
+				tasks.map((task) => {
+					if (task.id === id) {
+						return { ...task, done: !task.done };
+					}
+					return task;
+				})
+			);
+		};
+
+		const setAllDone = () => {
+			setTasks((tasks) =>
+				tasks.map((task) => ({
+					...task,
+					done: true
+				}))
+			);
+		};
+
+		return [
+			tasks,
+			addNewTask,
+			removeTask,
+			toggleTaskDone,
+			setAllDone
+		];
+	};
+
+	const [
+		tasks,
+		addNewTask,
+		removeTask,
+		toggleTaskDone,
+		setAllDone
+	] = useTasks();
+
+	const [hideDone, setHideDone] = useState(false);
 
 	const toggleHideDone = () => {
 		setHideDone((hideDone) => !hideDone);
-	};
-
-	const removeTask = (id) => {
-		setTasks((tasks) =>
-			tasks.filter((task) => task.id !== id)
-		);
-	};
-
-	const toggleTaskDone = (id) => {
-		setTasks((tasks) =>
-			tasks.map((task) => {
-				if (task.id === id) {
-					return { ...task, done: !task.done };
-				}
-				return task;
-			})
-		);
-	};
-
-	const setAllDone = () => {
-		setTasks((tasks) =>
-			tasks.map((task) => ({
-				...task,
-				done: true
-			}))
-		);
-	};
-
-	const addNewTask = (newTaskContent) => {
-		setTasks((tasks) => [
-			...tasks,
-			{
-				id: tasks.length
-					? tasks[tasks.length - 1].id + 1
-					: 1,
-				done: false,
-				content: newTaskContent
-			}
-		]);
 	};
 
 	return (
