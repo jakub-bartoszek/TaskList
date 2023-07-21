81325bc7a157f8a704795f59cde5880457f04ac9
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-07-21 16:00:26 +0200

    addTask from redux

--- src/features/tasks/Form/index.js
+++ src/features/tasks/Form/index.js
@@ -2,17 +2,32 @@ import { useState } from "react";
 import { Input } from "../../../common/Input/styled";
 import { Button, Wrapper } from "./styled";
 import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { addNewTask } from "../tasksSlice";
+import { nanoid } from "@reduxjs/toolkit";
 
 
-export const Form = ({ addNewTask }) => {
+export const Form = () => {
 
+  const resetInput = () => {
+    setNewTaskContent("");
+    inputRef.current.focus();
+  };
+
+  const dispatch = useDispatch();
   const inputRef = useRef(null);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    addNewTask(newTaskContent.trim());
-    setNewTaskContent("");
-    inputRef.current.focus();
+
+    if (newTaskContent.trim()) {
+      dispatch(addNewTask({
+        content: newTaskContent,
+        done: false,
+        id: nanoid(),
+      }));
+    }
+    resetInput();
   };
 
   const [newTaskContent, setNewTaskContent] = useState("");
