c723af9fcebbc7de33a9dde2b8cff5178a9201a0
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-07-21 15:58:50 +0200

    added saga

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,12 @@
+import { call, takeEvery, select, } from "@redux-saga/core/effects";
+import { selectTasks } from "./tasksSlice";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* saveTasksInLocalStorageHandler() {
+  const tasks = yield select(selectTasks);
+  yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+  yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
