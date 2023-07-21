f258cc7703ed1c8ffda1a2fad3cb19adb9287411
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-07-21 16:00:37 +0200

    working task buttons

--- src/features/tasks/TasksList/index.js
+++ src/features/tasks/TasksList/index.js
@@ -1,23 +1,47 @@
-import { List, Task, Content, ToggleDoneButton, EditButton, RemoveButton } from "./styled";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	List,
+	Task,
+	Content,
+	ToggleDoneButton,
+	EditButton,
+	RemoveButton
+} from "./styled";
+import {
+	removeTask,
+	selectHideDone,
+	selectTasks,
+	toggleTaskDone
+} from "../tasksSlice";
 
-export const TasksList = ({ tasks, removeTask, hideDone, toggleTaskDone }) => {
-  return (
-    <List>
-      {tasks.map((task) => (
-        <Task
-          key={task.id}
-          hidden={hideDone && task.done}
-        >
-          <ToggleDoneButton onClick={() => toggleTaskDone(task.id)}>{task.done ? "✔️" : ""}</ToggleDoneButton>
-          <Content
-            done={task.done}
-          >
-            {task.content}
-          </Content>
-          <EditButton>✏️</EditButton>
-          <RemoveButton onClick={() => removeTask(task.id)}>🗑️</RemoveButton>
-        </Task>
-      ))}
-    </List>
-  );
+export const TasksList = () => {
+	const tasks = useSelector(selectTasks);
+	const hideDone = useSelector(selectHideDone);
+	const dispatch = useDispatch();
+
+	return (
+		<List>
+			{tasks.map((task) => (
+				<Task
+					key={task.id}
+					hidden={hideDone && task.done}
+				>
+					<ToggleDoneButton
+						onClick={() =>
+							dispatch(toggleTaskDone(task.id))
+						}
+					>
+						{task.done ? "✔️" : ""}
+					</ToggleDoneButton>
+					<Content done={task.done}>{task.content}</Content>
+					<EditButton>✏️</EditButton>
+					<RemoveButton
+						onClick={() => dispatch(removeTask(task.id))}
+					>
+						🗑️
+					</RemoveButton>
+				</Task>
+			))}
+		</List>
+	);
 };
