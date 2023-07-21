86c72b34b575886573e00a6fd7d5c1ebdf505d84
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-07-21 15:57:45 +0200

    added store

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import tasksReducer from "./features/tasks/tasksSlice";
+import createSagaMiddleware from "@redux-saga/core";
+import { tasksSaga } from "./features/tasks/tasksSaga";
+
+const sagaMiddleWare = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+  },
+  middleware: [sagaMiddleWare]
+});
+
+sagaMiddleWare.run(tasksSaga);
+
+export default store;
