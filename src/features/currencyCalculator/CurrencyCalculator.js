d14ce342384ad6e24cc934cd45371066de2ddca8
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-07-25 17:00:22 +0200

    created second site

---
+++ src/features/currencyCalculator/CurrencyCalculator.js
@@ -0,0 +1,14 @@
+import { Container } from "../../common/Container/styled";
+import { Header } from "../../common/Header";
+import { Section } from "../../common/Section";
+
+function CurrencyCalculator() {
+  return (
+    <Container>
+      <Header title="Currency Calculator" />
+      <Section />
+    </Container>
+  );
+}
+
+export default CurrencyCalculator;
