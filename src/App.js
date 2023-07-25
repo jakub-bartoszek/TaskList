708044282269077ce98355803ad9a7640ee07715
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-07-25 20:54:25 +0200

    added redirect to to-do-list

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import TasksPage from "./features/tasks/TasksPage/TasksPage";
 import TaskPage from "./features/tasks/TaskPage/TaskPage";
 import CurrencyCalculator from "./features/currencyCalculator/CurrencyCalculator";
 import { StyledNavLink } from "./common/StyledNavLink/styled";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes, red } from "react-router-dom";
 import { Navigation } from "./common/Navigation/styled";
 
 const App = () => {
@@ -18,9 +18,10 @@ const App = () => {
         </StyledNavLink>
       </Navigation>
       <Routes>
-        <Route path={`/to-do-list/:id`} element={<TaskPage />} />
+        <Route path="/to-do-list/:id" element={<TaskPage />} />
         <Route path="/to-do-list" element={<TasksPage />} />
         <Route path="/currency-calculator" element={<CurrencyCalculator />} />
+        <Route path="/" element={<Navigate to="/to-do-list" />} />
       </Routes>
 
     </HashRouter>
