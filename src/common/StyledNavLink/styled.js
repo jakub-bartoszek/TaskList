6d9203fc6c81e7a03a224379604f7b886fc132cb
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-07-26 17:26:15 +0200

    debuged console error

--- src/common/StyledNavLink/styled.js
+++ src/common/StyledNavLink/styled.js
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-export const StyledNavLink = styled(NavLink).attrs(() => ("active"))`
+export const StyledNavLink = styled(NavLink)`
   color: white;
   display: flex;
   padding: 10px 20px;
