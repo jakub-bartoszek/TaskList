31c0daa823ef151b84a0f8a03f889cc0f504d64b
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-08-03 15:34:00 +0200

    restyled buttons in currency calculator

--- src/common/Input/styled.js
+++ src/common/Input/styled.js
@@ -1,14 +1,23 @@
 import styled from "styled-components";
 
 export const Input = styled.input`
-height: 25px;
-background-color: hsl(260, 5%, 11%);
-color: white;
-width: 100%;
-border: 2px solid hsl(260, 5%, 10%);
-border-radius: 10px;
-padding-left: 10px;
-&:active, &:focus{
-  outline: none;
-} 
+	height: 25px;
+	background-color: hsl(260, 5%, 11%);
+	color: white;
+	width: 100%;
+	border: 2px solid hsl(260, 5%, 10%);
+	border-radius: 10px;
+	padding-left: 10px;
+	&:active,
+	&:focus {
+		outline: none;
+	}
+	::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+	::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
 `;
---
+++ src/features/currencyCalculator/styled.js
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+export const CurrencyInput = styled.div`
+	width: 50%;
+	display: grid;
+	grid-template-columns: 1fr auto;
+`;
+
+export const DropDownContent = styled.div`
+z-index: 10;
+	display: flex;
+	position: absolute;
+	transition: all 0.3s;
+	transform-origin: 50% 0%;
+	transform: scale(1, 0);
+`;
+
+export const Button = styled.button`
+	height: 31px;
+	color: pink;
+	background-color: hsl(270, 4%, 11%);
+	border: 2px solid hsl(260, 5%, 10%);
+	border-radius: 10px;
+	transition: background-color 0.3s ease, border-radius 0.1s ease-out;
+	cursor: pointer;
+`;
+
+export const SubmitButton = styled.button`
+	margin-top: 15px;
+	width: 100%;
+	height: 31px;
+	color: hsl(260, 6%, 10%);
+	font-weight: 700;
+	background-color: hsl(0, 100%, 85%);
+	border: 2px solid hsl(260, 5%, 10%);
+	border-radius: 10px;
+	transition: 0.3s;
+	cursor: pointer;
+	&:hover{
+		background-color: #ffc0c0;
+	}
+`;
+
+export const Select = styled.div`
+	position: relative;
+	display: inline-block;
+	&:hover ${DropDownContent} {
+		display: flex;
+		transform: scale(1, 1);
+	}
+	&:hover ${Button} {
+		cursor: pointer;
+		border-radius: 10px 10px 0 0px;
+		background-color: hsl(270, 4%, 13%);
+	}
+`;
+
+export const Options = styled.div`
+	border: 2px solid hsl(260, 5%, 10%);
+	border-bottom: 1px solid hsl(260, 5%, 10%);
+	border-top: none;
+	border-radius: 0 0 10px 10px;
+`;
+
+export const Option = styled.button`
+	height: 23px;
+	background-color: hsl(270, 4%, 11%);
+	width: 100%;
+	text-align: left;
+	color: pink;
+	cursor: pointer;
+
+	border: none;
+	border-bottom: 1px solid hsl(260, 5%, 10%);
+	transition: 0.3s;
+	&:hover {
+		background-color: hsl(270, 4%, 13%);
+		text-shadow: 0px 0px 10px hsl(0, 100%, 88%);
+		color: hsl(0, 100%, 92%);
+	}
+	&:last-child {
+		border-radius: 0 0 7px 7px;
+	}
+`;
+
+export const CurrencyOutput = styled.div`
+	display: grid;
+	grid-template-columns: max-content max-content;
+	align-items: end;
+`;
+
+export const Output = styled.div`
+	margin: 0;
+	padding: 0;
+	height: 60px;
+	display: flex;
+	font-size: 60px;
+`;
