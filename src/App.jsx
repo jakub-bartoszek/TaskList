2044bc5ca30cf50ddff4670b0a3782c99f735758
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-11 17:57:43 +0100

    removed currency calculator due to api problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 import TasksPage from "./features/tasks/TasksPage/TasksPage";
 import TaskPage from "./features/tasks/TaskPage/TaskPage";
-import CurrencyCalculator from "./features/currencyCalculator/CurrencyCalculator";
-import { StyledNavLink } from "./common/StyledNavLink/styled";
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Navigation } from "./common/Navigation/styled";
 
 const App = () => {
 
   return (
     <HashRouter>
-      <Navigation>
-        <StyledNavLink to="/to-do-list">
-          To-do List
-        </StyledNavLink>
-        <StyledNavLink to="/currency-calculator">
-          Currency Calculator
-        </StyledNavLink>
-      </Navigation>
       <Routes>
         <Route path="/to-do-list/:id" element={<TaskPage />} />
         <Route path="/to-do-list" element={<TasksPage />} />
-        <Route path="/currency-calculator" element={<CurrencyCalculator />} />
         <Route path="/" element={<Navigate to="/to-do-list" />} />
       </Routes>
 
--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./features/tasks/tasksSlice";
-import resultReducer from "./features/currencyCalculator/currencySlice";
 import createSagaMiddleware from "@redux-saga/core";
 import { tasksSaga } from "./features/tasks/tasksSaga";
 
@@ -8,7 +7,6 @@ const sagaMiddleWare = createSagaMiddleware();
 
 const store = configureStore({
 	reducer: {
-		result: resultReducer,
 		tasks: tasksReducer
 	},
 	middleware: [sagaMiddleWare]
